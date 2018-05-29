2702701039b2ea7a8545cf2119df9f85a950f569
Aron Crawford <aroncrawford1029@yahoo.com>
2018-05-29 16:56:21 -0400

    tuesday afternoon

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"> 
+      <div> Welcome To Hooper </div>  
+      </div>
+    );
+  }
+}
+
+export default App;
