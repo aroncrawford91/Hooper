64db8bdabb764d8e3c3c62f8035e7c3afd303e3c
Aron Crawford <aroncrawford1029@yahoo.com>
2018-05-30 11:50:18 -0400

    wed am

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-      <div> Welcome To Hooper </div>  
+      <div> Welcome To Hooper </div>
+      <div>
+      {/* <img src={hooper} className="App-logo" alt="logo" /> */}
+      </div>   
       </div>
     );
   }
